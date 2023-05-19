3ca096df4a25e1d34a12f3fe0c7d30567fd11cf0
Hikaru Kurodq <h1kqaa@gmail.com>
2023-05-19 11:08:01 +0900

    first

---
+++ CISample2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CISample2
+//
+//  Created by Hikaru Kuroda on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
